3b502354fe7cee7b7a637425d4647451b0a9be5f
Zeeshan Rahman <129328705+Zeeshan-Ra@users.noreply.github.com>
2024-06-25 23:31:49 +0530

    page.js

--- src/app/myprofile/page.js
+++ src/app/myprofile/page.js
@@ -4,46 +4,39 @@ import CustomerHeader from "../_components/CustomerHeader";
 import Footer from "../_components/Footer";
 
 const Page = () => {
-    const [myOrders, setMyOrders] = useState([]);
+
+    const [myOrders, setMyOrders] = useState([])
 
     useEffect(() => {
-        const getMyOrders = async () => {
-            const userStorage = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user'));
-            if (userStorage) {
-                try {
-                    const response = await fetch(`http://localhost:3000/api/order?id=${userStorage._id}`);
-                    const data = await response.json();
-                    if (data.success) {
-                        setMyOrders(data.result);
-                    } else {
-                        console.error('Error fetching orders:', data.error);
-                    }
-                } catch (error) {
-                    console.error('Error fetching orders:', error);
-                }
-            }
-        };
-        getMyOrders();
-    }, []);
+        getMyOrders()
+    }, [])
+    
+    const getMyOrders = async () => {
+        const userStorage = localStorage.getItem('user') || JSON.parse(localStorage.getItem('user'))
+        let response = await fetch("http://localhost:3000/api/order?id=" + userStorage._id);
+        response = await response.json();
+        if (response.success) {
+            setMyOrders(response.result)
+        }
+    }
+
 
     return (
         <div>
             <CustomerHeader />
-            {myOrders.length > 0 ? (
-                myOrders.map((item, index) => (
-                    <div className="medshop-wrapper" style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: 28 }} key={index}>
-                        <h4>Name: {item.data?.name}</h4>
+            {
+                myOrders.map((item) => (
+                    <div className="medshop-wrapper" style={{marginLeft: 'auto', marginRight: 'auto', marginTop: 28}}>
+                        <h4>Name : {item.data.name}</h4>
                         <div>Amount: {item.amount}</div>
-                        <div>Address: {item.data?.address}</div>
+                        <div>Address: {item.data.address}</div>
                         <div>Status: {item.status}</div>
                     </div>
                 ))
-            ) : (
-                <div>No orders found.</div>
-            )}
+            }
             <Footer />
         </div>
-    );
-};
+    )
+}
 
 export default Page;
