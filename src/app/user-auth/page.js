3ce7318cda35baaf6501e87c5c7403ae4fff51bb
Zeeshan Rahman <129328705+Zeeshan-Ra@users.noreply.github.com>
2024-06-25 23:35:17 +0530

    page.js

--- src/app/user-auth/page.js
+++ src/app/user-auth/page.js
@@ -1,43 +1,33 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import CustomerHeader from "../_components/CustomerHeader";
 import Footer from "../_components/Footer";
 import UserLogin from "../_components/UserLogin";
 import UserSignup from "../_components/UserSignup";
 
-const Page = ({ searchParams }) => {
+
+const Page = (props) => {
+
     const [login, setLogin] = useState(true);
 
     const handleLogin = () => {
-        setLogin(!login);
-    };
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const userLoggedIn = localStorage.getItem('userLoggedIn');
-            setLogin(userLoggedIn === 'true');
-        }
-    }, []);
+        setLogin(!login)
+    }
 
     return (
         <div>
             <CustomerHeader />
             <div className="container">
-                {login ? (
-                    <UserLogin redirect={searchParams} />
-                ) : (
-                    <UserSignup redirect={searchParams} />
-                )}
-                <p style={{ color: 'gray', marginTop: 15 }}>
-                    {login ? "Do not have an account?" : "Already have an account?"}
-                    <span style={{ cursor: "pointer", color: 'blue' }} onClick={handleLogin}>
-                        {login ? " Sign up" : " Log in"}
-                    </span>
-                </p>
+            {
+                login ?
+                    <UserLogin redirect={props.searchParams} /> :
+                        <UserSignup redirect={props.searchParams} />
+            }
+            <p style={{color:'gray', marginTop:15}}>{login ? "Do not have account?":"Already have account?"}<span style={{cursor:"pointer",color:'blue'}} onClick={handleLogin}>{login? " Signup" : " Login"}</span></p>
             </div>
             <Footer />
         </div>
-    );
-};
+    )
+}
 
 export default Page;
