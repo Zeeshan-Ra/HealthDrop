7fa535af0cc66ff3eb7aef920ebded201519fe74
Zeeshan Rahman <129328705+Zeeshan-Ra@users.noreply.github.com>
2024-06-26 01:18:39 +0530

    db.js

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,2 +1,2 @@
-let {usernamee, passwordd} = process.env
-export const connectionStr = "mongodb+srv://"+usernamee+":"+passwordd+"@cluster0.qefkm0h.mongodb.net/testNext?retryWrites=true&w=majority&appName=Cluster0";
+// let {usernamee, passwordd} = process.env
+export const connectionStr = "mongodb+srv://zeeshu880:Zeeshan123@cluster0.qefkm0h.mongodb.net/testNext?retryWrites=true&w=majority&appName=Cluster0";
